aa1e4643e70f6ae3e7c7e4a962815d33a6655cb9
EpicProgger <adrian@t-online.de>
2018-01-24 22:36:04 +0100

    adds placing of elements

--- js/placing.js
+++ js/placing.js
@@ -1,49 +1,55 @@
-
-
 var eventArray;
 var events;
 var container = document.getElementById('event-container');
 
 addEvents();
+placeEvents();
 
 function createArray() {
-    function el(start, end, name, dataevent) {
-        this.start = start;
-        this.end = end;
+    function el(starth, startmin, endh, endmin, name, dataevent) {
+        this.starthour = starth;
+        this.startminute = startmin;
+        this.endhour = endh;
+        this.endmin = endmin;
         this.name = name;
         this.dataevent = dataevent;
     }
 
     eventArray = [
-        new el('12:30', '13:30', 'Rowing', 'event-1'),
-        new el('15:30', '16:30', 'Yoga', 'event-2')
+        new el(12, 30, 15, 15, 'Rowing', 'event-1'),
+        new el(15, 30, 16, 30, 'Yoga', 'event-2')
     ];
 }
 
 
-
 function addEvents() {
     createArray();
-    container.innerHTML += '<span></span>';
-    for(var i = 0; i < eventArray.length; i++){
+    for (var i = 0; i < eventArray.length; i++) {
         var li = document.createElement('li');
-        console.dir(eventArray[i]);
+
         li.setAttribute('class', 'single-event');
-        li.setAttribute('start', eventArray[i].start);
-        li.setAttribute('end', eventArray[i].end);
+        //li.setAttribute('start', eventArray[i].start);
+        //li.setAttribute('end', eventArray[i].end);
         li.setAttribute('data-event', eventArray[i].dataevent);
         li.innerHTML = '<em class="event-name">' + eventArray[i].name + '</em>';
         container.appendChild(li);
-        container.innerHTML += '<div></div>';
     }
-    container.innerHTML += 'lel';
-    console.log('this happens.')
+
+    events = document.getElementsByClassName('single-event');
+    console.dir(events);
+    console.dir(eventArray);
+
+
+    //console.log('this happens.')
 
 }
 
-//function placeEvents(){
-//    container.innerHTML += 'HDHDHD';
-//}
-
-
-
+function placeEvents() {
+    for (var i = 0; i < events.length; i++) {
+        var topoffset = (eventArray[i].starthour - 9) * 100 + eventArray[i].startminute * 100/60 - 1;
+        var elemheight = ((eventArray[i].endhour * 60 + eventArray[i].endmin) - (eventArray[i].starthour*60 + eventArray[i].startminute))*100/60
+        console.log(elemheight);
+        events[i].style.top = topoffset + 'px';
+        events[i].style.height = elemheight + 'px';
+    }
+}
